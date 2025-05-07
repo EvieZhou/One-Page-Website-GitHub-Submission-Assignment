0ddc3ea5359df7a82d5fcea03986350a9f406ea2
EvieZhou <87358629+EvieZhou@users.noreply.github.com>
2025-05-07 16:07:10 -0700

    Update one_page_website.js

--- one_page_website.js
+++ one_page_website.js
@@ -1,1 +1,12 @@
 
+<div class="gallery">
+  <img src="images/thumb1.jpg" alt="Image 1" class="thumbnail" data-src="images/large1.jpg">
+  <img src="images/thumb2.jpg" alt="Image 2" class="thumbnail" data-src="images/large2.jpg">
+  <img src="images/thumb3.jpg" alt="Image 3" class="thumbnail" data-src="images/large3.jpg">
+</div>
+
+<!-- Lightbox Modal -->
+<div id="lightbox" class="lightbox">
+  <span class="close">&times;</span>
+  <img class="lightbox-content" id="lightbox-img">
+</div>
